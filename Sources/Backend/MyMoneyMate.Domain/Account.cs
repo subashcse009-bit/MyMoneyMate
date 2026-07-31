08b1d97a71a25d2a544e1e7308c33d7f5b419f35
subashcse009-bit <subashcse009@gmail.com>
2026-08-01 00:22:39 +0530

    Domain Classes are created

---
+++ Sources/Backend/MyMoneyMate.Domain/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyMate.Domain
+{
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+        [Required]
+        public string AccountName { get; set; }
+        [Required]
+        public string AccountType { get; set; }
+        [Required]
+        public decimal OpeningBalance {  get; set; }
+        public decimal CreditLimit { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        public int DisplayOrder {  get; set; }
+
+    }
+}
---
+++ Sources/Backend/MyMoneyMate.Domain/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyMate.Domain
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public int NSWID { get; set; }
+        [Required]
+        public string TransactionTypeID { get; set; }
+        [Required]
+        public decimal Budget { get; set; }
+        [Required]
+        public bool IncludeExpense { get; set; }
+        [Required]
+        public bool IncludeIncome { get; set; }
+        [Required]
+        public bool IncludeSavings { get; set; }
+
+    }
+}
