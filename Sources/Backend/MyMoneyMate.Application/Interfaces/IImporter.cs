3bcc3af679e33cc1010f2ddc6208ef126ad45558
subashcse009-bit <subashcse009@gmail.com>
2026-08-20 23:11:11 +0530

    Missed files added

---
+++ Sources/Backend/MyMoneyMate.Application/Interfaces/IImporter.cs
@@ -0,0 +1,15 @@
+﻿using MyMoneyMate.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyMate.Application.Interfaces
+{
+    public interface IImporter
+    {
+        bool CanImport(string fileName, string? contentType = null);
+        IEnumerable<TransactionImportRow> ReadTransactions(Stream stream);
+    }
+}
