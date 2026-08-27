c54066804bd1c2669fba188f4e922ae3830ee755
subashcse009-bit <subashcse009@gmail.com>
2026-08-27 22:34:52 +0530

    Account Controller

---
+++ Sources/Backend/MyMoneyMate.API/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMoneyMate.Application.Services;
+
+namespace MyMoneyMate.API.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountService _service;
+
+        public AccountController(AccountService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList()
+        {
+            var accounts = await _service.GetList();
+
+            return Ok(new
+            {
+                Accounts = accounts
+            });
+        }
+    }
+}
---
+++ Sources/Backend/MyMoneyMate.Application/Services/AccountService.cs
@@ -0,0 +1,28 @@
+﻿using MyMoneyMate.Application.Repository.IRepository;
+using MyMoneyMate.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyMate.Application.Services
+{
+    public class AccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public AccountService(IAccountRepository accountRepository, ICategoryRepository categoryRepository)
+        {
+            _accountRepository = accountRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<Account>>  GetList()
+        {
+            var accounts = await _accountRepository.GetAllAsync();
+            return accounts;
+        }
+    }
+}
